0e426c4ce0669d7e1c87a98e7b7a15a6301b7239
Metalcry1 <danielserrano.dev@gmail.com>
2024-09-14 12:37:22 +0200

    Fix Repository-remoteDataSource

--- app/src/main/java/com/codingpit/pvpcplanner/data/Repository.kt
+++ app/src/main/java/com/codingpit/pvpcplanner/data/Repository.kt
@@ -1,23 +1,7 @@
 package com.codingpit.pvpcplanner.data
 
-import android.util.Log
-import com.codingpit.pvpcplanner.data.remote.RemoteDataSource
 import com.codingpit.pvpcplanner.domains.models.PVPCModel
-import javax.inject.Inject
 
-class Repository @Inject constructor (
-    private val remoteDataSource: RemoteDataSource,
-) {
-    suspend fun getPrices(): List<PVPCModel> {
-        try {
-            Log.d("Repository", "Fetching data...")
-            val responseRemote = remoteDataSource.getPrices()
-            Log.d("Repository", "data fetched successfully")
-            Log.w("Repository", responseRemote.toString())
-            return responseRemote
-        } catch (e: Exception) {
-            Log.e("Repository", "Error fetching data", e)
-            return emptyList() // Or handle the error as needed
-        }
-    }
+interface Repository{
+    suspend fun getPrices(): List<PVPCModel>
 }
---
+++ app/src/main/java/com/codingpit/pvpcplanner/data/RepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.codingpit.pvpcplanner.data
+
+import android.util.Log
+import com.codingpit.pvpcplanner.data.remote.RemoteDataSource
+import com.codingpit.pvpcplanner.domains.models.PVPCModel
+import javax.inject.Inject
+
+class RepositoryImpl @Inject constructor (
+    private val remoteDataSource: RemoteDataSource,
+): Repository {
+    override suspend fun getPrices(): List<PVPCModel> {
+        try {
+            Log.d("Repository", "Fetching data...")
+            val responseRemote = remoteDataSource.getPrices()
+            Log.d("Repository", "data fetched successfully")
+            Log.w("Repository", responseRemote.toString())
+            return responseRemote
+        } catch (e: Exception) {
+            Log.e("Repository", "Error fetching data", e)
+            return emptyList() // Or handle the error as needed
+        }
+    }
+}
